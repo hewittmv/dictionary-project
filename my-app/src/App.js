735fb13090b83c4054c59b7655c6467a66a75243
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-10-07 15:12:16 +0100

    cleared app.js & added github link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="github-link"
+          href="https://github.com/hewittmv/dictionary-project"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open Source Code by VHM
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
